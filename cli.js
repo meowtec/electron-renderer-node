b6b286accd8ee1a6505e0cdd8830e6cd2ed800ac
meowtec <bertonzh@gmail.com>
2018-07-03 18:02:54 +0800

    feat: Add command options `window` for opening the main window

--- cli.js
+++ cli.js
@@ -4,10 +4,16 @@ const argv = require('yargs')
   .command('[options] [file]', 'Execute a script or starts a REPL')
   .option('inspect', {
     default: false,
-    describe: 'Open the inspector'
+    describe: 'Open the inspector',
+  })
+  .option('window', {
+    default: false,
+    describe: 'Show browser window',
   })
   .alias('help', 'h')
   .alias('version', 'v')
+  .alias('inspect', 'i')
+  .alias('window', 'w')
   .argv
 
 require('./lib/')(argv)
